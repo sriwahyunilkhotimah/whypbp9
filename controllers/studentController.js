eadfcce841f2f2b0e7b62e9c6af31cd734539899
sriwahyunilkhotimah <114728068+sriwahyunilkhotimah@users.noreply.github.com>
2023-12-06 23:47:04 +0700

    Add files via upload

---
+++ controllers/studentController.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
+// GET mahasiswa
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM mahasiswa', (error, results) => {
+    if (error) {
+      console.error('Error fetching mahasiswa:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// GET /mahasiswa/:nim
+router.get('/:nim', (req, res) => {
+  const mahasiswaNim = req.params.nim;
+  db.query('SELECT * FROM mahasiswa WHERE nim = ?', [mahasiswaNim], (error, results) => {
+    if (error) {
+      console.error('Error fetching mahasiswa:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    } else if (results.length === 0) {
+      res.status(404).json({ message: 'Mahasiswa not found' });
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+// PUT /mahasiswa/:nim
+router.put('/:nim', (req, res) => {
+  const mahasiswaNim = req.params.nim;
+  const { nama, gender, prodi, alamat } = req.body;
+  db.query(
+    'UPDATE mahasiswa SET nama = ?, gender = ?, prodi = ?, alamat = ? WHERE nim = ?',
+    [nama, gender, prodi, alamat, mahasiswaNim],
+    (error) => {
+      if (error) {
+        console.error('Error updating mahasiswa:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+      } else {
+        res.json("Updating mahasiswa successfully");
+      }
+    }
+  );
+});
+
+module.exports = router;
